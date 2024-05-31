1d1b52748113e60ffbe6d423547b777a6015f2a4
HassanKhanKhan <46518068+HassanKhanKhan@users.noreply.github.com>
2024-06-01 02:29:00 +0400

    Update server.js

--- server.js
+++ server.js
@@ -155,7 +155,8 @@ MongoClient.connect(connectionString, (err, client) => {
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(Server is running on port ${PORT});
+      console.log(`Server is running on port ${PORT}`);
+
     });
   }
 });
